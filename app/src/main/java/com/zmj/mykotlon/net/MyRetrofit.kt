add014d078e7732cd6ad2342e50a4203def167db
ZmjDns <292570927@qq.com>
2019-06-05 22:37:47 +0800

    retrofit

---
+++ app/src/main/java/com/zmj/mykotlon/net/MyRetrofit.kt
@@ -0,0 +1,15 @@
+package com.zmj.mykotlon.net
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2019/6/5
+ * Description :
+ */
+object MyRetrofit {
+
+    fun getRetrofit(){
+
+    }
+}
