06821e70f9749965788f85412799f8e11dbfdbc1
ZmjDns <292570927@qq.com>
2019-06-19 09:13:28 +0800

    baseAct实现

--- app/src/main/java/com/zmj/mykotlon/ui/activity/BaseActivity.kt
+++ app/src/main/java/com/zmj/mykotlon/ui/activity/BaseActivity.kt
@@ -1,8 +1,12 @@
 package com.zmj.mykotlon.ui.activity
 
+import android.content.Context
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Window
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 
 /**
  * Author : Zmj
@@ -13,8 +17,33 @@ import android.support.v7.app.AppCompatActivity
  */
 abstract class BaseActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private val imm :InputMethodManager by lazy {
+        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        setContentView(setLayoutId())
+
+    }
+    protected abstract fun setLayoutId() : Int
+
+
+    override fun finish(){
+        if (!isFinishing){
+            super.finish()
+            hideSoftKeyBord()
+        }
+    }
+
+
+    private fun hideSoftKeyBord(){
+        currentFocus?.let {
+            imm.hideSoftInputFromWindow(it.windowToken,2)
+        }
     }
 
 
