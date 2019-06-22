ebc8064cd72b49d7bc195a7b422e7d230eecf18c
ZmjDns <292570927@qq.com>
2019-06-22 22:36:34 +0800

    监听数据库版本更新

--- app/src/main/java/com/zmj/mykotlon/MyApplication.kt
+++ app/src/main/java/com/zmj/mykotlon/MyApplication.kt
@@ -2,8 +2,11 @@ package com.zmj.mykotlon
 
 import android.app.Application
 import android.database.sqlite.SQLiteDatabase
+import com.zmj.mykotlon.entry.LawItem
+import org.litepal.LitePal
 import org.litepal.LitePalApplication
 import org.litepal.tablemanager.Connector
+import org.litepal.tablemanager.callback.DatabaseListener
 
 /**
  * Author : Zmj
@@ -16,6 +19,17 @@ class MyApplication: LitePalApplication() {
     var sqlDB : SQLiteDatabase? = null
     override fun onCreate() {
         super.onCreate()
+
+        LitePal.registerDatabaseListener(object : DatabaseListener{
+            override fun onCreate() {
+                loge("MyApplication","execute OnCreate()")
+            }
+
+            override fun onUpgrade(oldVersion: Int, newVersion: Int) {
+                loge("MyApplication","oldVersion:$oldVersion,newVersion:$newVersion")
+                //LitePal.deleteAll(LawItem::class.java)
+            }
+        })
 
         sqlDB = Connector.getDatabase()
     }
