a899c29ab1f28da826c860a491f985af82381f65
ZmjDns <292570927@qq.com>
2019-06-24 22:36:54 +0800

    lambad表达式简单使用

--- app/src/main/java/com/zmj/mykotlon/mixture/TestWebviewAct.kt
+++ app/src/main/java/com/zmj/mykotlon/mixture/TestWebviewAct.kt
@@ -6,6 +6,7 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.zmj.mykotlon.R
 import com.zmj.mykotlon.ui.activity.BaseActivity
+import kotlinx.android.synthetic.main.act_test_webview.*
 
 /**
  * Author : Zmj
@@ -16,7 +17,7 @@ import com.zmj.mykotlon.ui.activity.BaseActivity
  */
 class TestWebviewAct : BaseActivity() {
 
-    private val wv_web : WebView by lazy {
+    private val webView : WebView by lazy {
         wv_web
     }
 
@@ -27,13 +28,26 @@ class TestWebviewAct : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //1.开启kotlin与H5通信的开关
-        wv_web.settings.javaScriptEnabled = true
-        //设置两个WebViewClient
-        wv_web.webViewClient = MyWebvieClient()
-        wv_web.webChromeClient = MyWebChromeClient()
+        setWebView()
+    }
+    //lambad表达式
+    //var add = {a: Int,b:Int -> a + b}
 
-        wv_web.loadUrl("https://baidu.com")
+    var setWebView = {
+        //1.开启kotlin与H5通信的开关
+        webView.settings.javaScriptEnabled = true
+        //2.设置两个WebViewClient
+        webView.webViewClient = MyWebvieClient()
+        webView.webChromeClient = MyWebChromeClient()
+
+        //kotlin与H5的通信方式1：H5调用kotlin
+        //设置h5与kotlin通信的桥梁类
+        //webView.addJavascriptInterface(对象，字符串) ： 对象.方法名,  字符串就是参数1对象的别名
+        webView.addJavascriptInterface(JavaScriptMethod(this),"jsInterface")
+
+
+        //3.加载网页
+        webView.loadUrl("https://baidu.com")
     }
 
     private class MyWebvieClient : WebViewClient(){
