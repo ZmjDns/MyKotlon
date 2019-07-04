098d86d92f6a94b9ad2baba8bd331e59e1021670
ZmjDns <292570927@qq.com>
2019-07-04 12:21:17 +0800

    基础CoroutineActivity

---
+++ app/src/main/java/com/zmj/mykotlon/coroutine/BaseScopedActivity.kt
@@ -0,0 +1,21 @@
+package com.zmj.mykotlon.coroutine
+
+import android.support.v7.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * DitHub : https://github.com/ZmjDns
+ * Time : 2019/7/3
+ * Description :
+ */
+abstract class BaseScopedActivity : AppCompatActivity(),CoroutineScope by MainScope() {
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cancel()        //销毁Activity对象时取消协程
+    }
+}
