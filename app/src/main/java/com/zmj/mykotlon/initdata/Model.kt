c41add4a108d1bb22082ddff39aaaf3b9a165080
ZmjDns <292570927@qq.com>
2019-07-10 22:53:10 +0800

    协程应用

--- app/src/main/java/com/zmj/mykotlon/initdata/Model.kt
+++ app/src/main/java/com/zmj/mykotlon/initdata/Model.kt
@@ -1,15 +1,11 @@
 package com.zmj.mykotlon.initdata
 
-import android.provider.Contacts
 import com.zmj.mykotlon.entry.LawItem
 import com.zmj.mykotlon.entry.Rid
+import com.zmj.mykotlon.utils.loge
 import com.zmj.mykotlon.net.IResponse
 import com.zmj.mykotlon.net.MyRetrofit
-import com.zmj.mykotlon.tryCatch
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,11 +17,13 @@ import java.lang.Exception
  * DitHub : https://github.com/ZmjDns
  * Time : 2019/6/17
  * Description : 获取基础数据Model
+ *
+ * https://mp.weixin.qq.com/s/UDVTuuTCHYnujlsaf9r8gA
  */
 class Model : IBaseData.IModel{
     override fun getRid(listener: IResponse<ArrayList<Rid>>) {
         try{
-            GlobalScope.launch {
+            GlobalScope.launch{
                 val call : Call<ArrayList<Rid>> = MyRetrofit.service.getRid()
 
                 call.enqueue(object : Callback<ArrayList<Rid>>{
@@ -41,7 +39,7 @@ class Model : IBaseData.IModel{
         }catch (e : Exception){
             e.printStackTrace()
         }
-
+//        //封装了tryCatch
 //        GlobalScope.launch {
 //            async{
 //                tryCatch({
@@ -54,7 +52,25 @@ class Model : IBaseData.IModel{
 //                }
 //            }
 //        }
+        //懒汉式启动携程
+        var job = GlobalScope.launch (start = CoroutineStart.LAZY){
+            loge("Test", "hahha")
+        }
+        job.start()
+
+        GlobalScope.launch {
+            job.join()  //等待job完成
+        }
+
+        var mainScope = MainScope()
+        mainScope.launch {
+            val aa = async(Dispatchers.IO) {
+
+            }.await()
+        }
     }
+
+
 
     override fun getLaw(listener: IResponse<ArrayList<LawItem>>) {
         try {
@@ -79,6 +95,5 @@ class Model : IBaseData.IModel{
                 listener.onFailed(t)
             }
         })
-
     }
 }
